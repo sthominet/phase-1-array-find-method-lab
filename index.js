0987637852b1ca2f375b302090c34d44d8a96ba5
Sandrine <sandrine.thominet@gmail.com>
2022-04-01 12:35:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(array) {
+const record = array.find(function(game){
+    return game.result === `W`
+})
+if (record === undefined) {
+    return undefined
+}
+else {
+    return record.year;
+}
+}
